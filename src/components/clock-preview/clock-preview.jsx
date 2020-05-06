37182e5dcf3db19c87aed8ace84edcbf1ac83db5
0zzycodes <horlalekan123@gmail.com>
2020-05-06 14:52:20 +0800

    added new clock and seconds count

--- src/components/clock-preview/clock-preview.jsx
+++ src/components/clock-preview/clock-preview.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './clock-preview.scss';
-const ClockPreview = ({ data: { hour, min } }) => {
+const ClockPreview = ({ data, otherTime }) => {
+  const { hour, min, sec } = data;
   return (
     <div className="clock-preview">
-      <h2 className="hour">{hour} :</h2>
-      <h2 className="min">
-        {min}
-        <span className="ampm">{'AM'}</span>
-      </h2>
+      <div
+        className="roller"
+        style={{ transform: `translate(-50%, -50%) rotate( ${sec * 6}deg)` }}
+      />
+      <div className="overlay">
+        {data.ampm ? (
+          <>
+            <h2 className="hour">{hour} :</h2>
+            <h2 className="min">
+              {min}
+              <span className="ampm">{data.ampm}</span>
+            </h2>
+          </>
+        ) : (
+          <>
+            <h2 className="hour">{hour} :</h2>
+            <h2 className="min">
+              {min}
+              <span className="ampm">{}</span>
+            </h2>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/sections/Clock/Clock.jsx
+++ src/sections/Clock/Clock.jsx
@@ -3,10 +3,28 @@ import './Clock.scss';
 import ClockPreview from '../../components/clock-preview/clock-preview';
 import add from '../../assets/add.svg';
 const Clock = () => {
-  const [userDate, setUserDate] = useState({ hour: '', min: '', sec: '' });
+  const [userDate, setUserDate] = useState({
+    hour: '',
+    min: '',
+    sec: ''
+  });
+  const [otherTime, setOtherTime] = useState({ hour: '', min: '', ampm: '' });
   useEffect(() => {
     setInterval(() => {
       let date = new Date();
+      const options = {
+          timeZone: 'Europe/London',
+          hour: 'numeric',
+          minute: 'numeric'
+        },
+        formatter = new Intl.DateTimeFormat([], options).format(new Date());
+      console.log(formatter.split(' ')[0].split(':'));
+
+      setOtherTime({
+        hour: formatter.split(' ')[0].split(':')[0],
+        min: formatter.split(' ')[0].split(':')[1],
+        ampm: formatter.split(' ')[1]
+      });
       setUserDate({
         hour: date.getHours(),
         min: date.getMinutes(),
@@ -14,20 +32,19 @@ const Clock = () => {
       });
     }, 1000);
   }, []);
-
-  const getDifferentTimeZoneTime = () => {
-    const options = {
-        timeZone: 'Europe/London',
-        hour: 'numeric',
-        minute: 'numeric'
-        // second: 'numeric'
-      },
-      formatter = new Intl.DateTimeFormat([], options).format(new Date());
-    console.log(formatter);
-  };
   return (
     <div className="clock">
-      <ClockPreview data={{ hour: userDate.hour, min: userDate.min }} />
+      <ClockPreview
+        data={{ hour: userDate.hour, min: userDate.min, sec: userDate.sec }}
+      />
+      <ClockPreview
+        data={{
+          hour: otherTime.hour,
+          min: otherTime.min,
+          sec: userDate.sec,
+          ampm: otherTime.ampm
+        }}
+      />
       <div className="add">
         <img src={add} alt="add" />
       </div>
